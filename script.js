c1851cc309ae29596b4964517581cd3ec60d2821
Kaleb-N <nwachukwukaleb@gmail.com>
2022-09-28 15:33:08 -0700

    updated

--- script.js
+++ script.js
@@ -4,56 +4,65 @@ let navbar = document.querySelector('.navbar');
 menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
-}
-//active when scrolling
+};
+
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
+    section.forEach(sec =>{
 
-
-    //active when scrolling
-  section.forEach(sec =>{
-
-    let top = window.scrollY;
-    let height = sec.offsetHeight;
-    let offset = sec.offsetTop - 150;
-    let id = sec.getAttribute('id');
-
-    if(top >= offset && top < offset + height){
-      navLinks.forEach(links =>{
-        links.classList.remove('active');
-        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
-      });
-    };
-
-  });
-
-
+      let top = window.scrollY;
+      let height = sec.offsetHeight;
+      let offset = sec.offsetTop - 150;
+      let id = sec.getAttribute('id');
+  
+      if(top >= offset && top < offset + height){
+        navLinks.forEach(links =>{
+          links.classList.remove('active');
+          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        });
+      };
+  
+    });
 }
 
 document.querySelector('#search-icon').onclick = () =>{
-  document.querySelector('#search-form').classList.toggle('active');
-}
+    document.querySelector('#search-form').classList.toggle('active');
+};
 document.querySelector('#close').onclick = () =>{
-  document.querySelector('#search-form').classList.remove('active');
-}
+    document.querySelector('#search-form').classList.remove('active');
+};
 
 var swiper = new Swiper(".home-slider", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 7500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  loop:true,
+    slidesPerView: 1,
+    spaceBetween: 300,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+      },
+      750: {
+        slidesPerView: 1,
+        
+      },
+      1024: {
+        slidesPerView: 1,
+        
+      },
+    },
 });
 
 var swiper = new Swiper(".review-slider", {
@@ -81,13 +90,3 @@ var swiper = new Swiper(".review-slider", {
 });
 
 
-// loader
-function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-}
-  
-function fadeOut(){
-    setInterval(loader, 2500);
-}
-  
-window.onload = fadeOut;
